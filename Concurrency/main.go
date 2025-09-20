282ff06a358b1e30d3bec2e9c8c20a831e88e050
katara-Jayprakash <katarajayprakash@icloud.com>
2025-09-20 13:18:08 +0530

    optimised the code to get all thread busy
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -1,18 +1,24 @@
+/*
+	 => thread with unfairness;
+		Total prime numbers up to 100000000: 5761455
+	  Time taken: 4.048066s
+*/
+/*
 package main
-
-import (
+  import (
 	"fmt"
 	"math"
 	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
-)
+  )
 
 var MaxInt int64 = 100000000
 var totalPrimeNum int64 = 0
 var Concurrency = 10
 
+//checking Prime number
 func checkPrime(x int) {
 	if x < 2 {
 		return
@@ -32,7 +38,7 @@ func checkPrime(x int) {
 	}
 	atomic.AddInt64(&totalPrimeNum, 1)
 }
-
+	// performing batch operation,
 func doBatch(id string, wg *sync.WaitGroup, start, end int) {
 	defer wg.Done()
 
@@ -55,6 +61,7 @@ func main() {
 
 	batchSize := MaxInt / int64(Concurrency) //100000000/10=> 10000000
 
+	// dividing task into batches of 10;
 	for i := 0; i < Concurrency; i++ {
 		wg.Add(1)
 
@@ -70,11 +77,79 @@ func main() {
 
 	wg.Wait()
 
-
 	fmt.Printf("Total prime numbers up to %d: %d\n", MaxInt, totalPrimeNum)
 	fmt.Printf("Time taken: %s\n", time.Since(startTime))
-	/*
-	Total prime numbers up to 100000000: 5761455
-  Time taken: 4.048066s
-	*/
+}
+*/
+
+/*optimising the logic more*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var MaxInt = 100000000
+var Concurrency = 10
+var totalPrimeNum int32 = 0
+var currentNumber int32 = 1 
+
+func checkPrime(x int) {
+	if x < 2 {
+		return
+	}
+	if x == 2 {
+		atomic.AddInt32(&totalPrimeNum, 1)
+		return
+	}
+	if x&1 == 0 {
+		return // skip even numbers
+	}
+
+	sqrtX := int(math.Sqrt(float64(x)))
+	for i := 3; i <= sqrtX; i += 2 {
+		if x%i == 0 {
+			return
+		}
+	}
+
+	atomic.AddInt32(&totalPrimeNum, 1)
+}
+
+func doWork(id string, w *sync.WaitGroup) {
+	defer w.Done()
+
+	start := time.Now()
+
+	for {
+		x := atomic.AddInt32(&currentNumber, 1)
+		if x > int32(MaxInt) {
+			break
+		}
+		checkPrime(int(x))
+	}
+
+	fmt.Printf("Thread %s completed in %s\n", id, time.Since(start))
+}
+
+func main() {
+	start := time.Now()
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < Concurrency; i++ {
+		wg.Add(1)
+		go doWork(strconv.Itoa(i), &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Total prime numbers up to %d: %d\n", MaxInt, totalPrimeNum)
+	fmt.Printf("Time taken: %s\n", time.Since(start))
 }
