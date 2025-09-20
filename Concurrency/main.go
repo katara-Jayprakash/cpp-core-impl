023e7191aee0135ba0413aada908f111d1905e09
katara-Jayprakash <katarajayprakash@icloud.com>
2025-09-20 12:10:14 +0530

    experimenting with 10thread
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -3,43 +3,78 @@ package main
 import (
 	"fmt"
 	"math"
+	"strconv"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 var MaxInt int64 = 100000000
 var totalPrimeNum int64 = 0
+var Concurrency = 10
 
 func checkPrime(x int) {
 	if x < 2 {
 		return
 	}
 	if x == 2 {
-		totalPrimeNum++
+		atomic.AddInt64(&totalPrimeNum, 1)
 		return
 	}
-	if x&1 == 0 { // x is even, so not prime
+	if x&1 == 0 {
 		return
 	}
-	// Correct square root calculation
 	sqrtX := int(math.Sqrt(float64(x)))
-
-	for i := 3; i <= sqrtX; i++ {
+	for i := 3; i <= sqrtX; i += 2 {
 		if x%i == 0 {
 			return
 		}
 	}
-	// If no divisor found, it's prime
-	totalPrimeNum++
+	atomic.AddInt64(&totalPrimeNum, 1)
+}
+
+func doBatch(id string, wg *sync.WaitGroup, start, end int) {
+	defer wg.Done()
+
+	startTime := time.Now()
+
+	for i := start; i <= end; i++ {
+		checkPrime(i)
+	}
+
+	duration := time.Since(startTime)
+
+	fmt.Printf("Thread %s: [%d - %d] complete in %s\n", id, start, end, duration)
 }
 
 
 func main() {
- start := time.Now()
+	startTime := time.Now()
 
- for i:=3;i<int(MaxInt); i++{
-	checkPrime(i);
- }
+	var wg sync.WaitGroup
 
- fmt.Println("checking till", MaxInt,"found", totalPrimeNum, "prime numbers.took",time.Since(start)); 
+	batchSize := MaxInt / int64(Concurrency) //100000000/10=> 10000000
 
+	for i := 0; i < Concurrency; i++ {
+		wg.Add(1)
+
+		start := int(i*int(batchSize)) + 1. // 1;
+		end := int((i + 1) * int(batchSize))  // 10000000
+
+		if i == Concurrency-1 {
+			end = int(MaxInt)
+		}
+
+		go doBatch( strconv.Itoa(i),  &wg, start, end)
+	}
+
+	wg.Wait()
+
+
+	fmt.Printf("Total prime numbers up to %d: %d\n", MaxInt, totalPrimeNum)
+	fmt.Printf("Time taken: %s\n", time.Since(startTime))
+	/*
+	Total prime numbers up to 100000000: 5761455
+  Time taken: 4.048066s
+	*/
 }
