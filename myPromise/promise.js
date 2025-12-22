911ef379c8dead0d9d71341a244da67751da4fce
katara-Jayprakash <katarajayprakash@icloud.com>
2025-12-22 23:37:23 +0530

    done something idkn
    Signed-off-by: katara-Jayprakash <katarajayprakash@icloud.com>

--- myPromise/promise.js
+++ myPromise/promise.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 
 class myPromise{
-      
+
    constructor(fn){
-     //lexical scoping ; 
+     //lexical scoping ;
      const afterDone = ((data)=>{this.resolve(data)})
      const notDone  =(() =>{})
-       
-     fn(afterDone,notDone);    
+
+     fn(afterDone,notDone);
   }
   then(cb){
-      this.resolve = cb; 
+      this.resolve = cb;
   }
 }
 
@@ -26,7 +26,6 @@ function callback(data){
 console.log(`everything is fine we finallly made it , Promise class! ${data}` )}
 
 
-let p = new myPromise(fun); 
+let p = new myPromise(fun);
 
 p.then(callback);
-
